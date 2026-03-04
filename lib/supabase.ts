78f1a6efd258ba2637c364532efbbeb738b009d4
Lucas Mendonça <seuemail@dominio.com>
2026-03-04 20:24:21 -0300

    Corrigindo chaves do Supabase e forçando build

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+const supabaseUrl = 'https://jovibfyswqndgxrfafrh.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvdmliZnlzd3FuZGd4cmZhZnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NzAwMzQsImV4cCI6MjA4ODA0NjAzNH0.laRw1M5mXx9ttT0PYzn275lOyVJ11XxZ94IaWYCiPbU'
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    // sessionStorage faz com que o login expire ao fechar a página!
+    // sessionStorage faz com que o login expire ao fechar a aba/navegador!
     storage: typeof window !== 'undefined' ? window.sessionStorage : undefined,
     autoRefreshToken: true,
     persistSession: true,
